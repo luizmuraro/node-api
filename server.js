2fa41a2196b29bf442156a932a09543c746d6900
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2020-02-12 18:04:04 -0300

    adiciona cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
+
 // Permite que a aplicação receba dados no formato de JSON
 app.use(express.json());
+app.use(cors());
+
 // Iniciando o DB
 mongoose.connect(
     "mongodb://localhost:27017/nodeapi",{ 
